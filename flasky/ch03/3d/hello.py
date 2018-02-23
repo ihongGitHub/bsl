e433f20814c3767802e880858b6f4385137b6ffc
ihong <ihong@uttec.co.kr>
2018-02-23 18:41:48 +0900

    bsl180223_1840

---
+++ flasky/ch03/3d/hello.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_script import Manager
+
+app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+manager = Manager(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+if __name__ == '__main__':
+    # app.run(debug=True)
+    manager.run()
+# python3 hello.py runserver --host 0.0.0.0
